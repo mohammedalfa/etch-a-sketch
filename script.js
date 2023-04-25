dc2e3d6cd8ec1dd3f30404e51c13e61ea60bc31f
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-24 21:25:33 -0400

    add header and selector functions

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const container = document.querySelector("#container");
-let color = "#aaa";
-let squares = 50;
+let color = "black";
+let squares = 0;
 let clicked = false;
+
+
+squareRender = document.querySelector("#render");
+squareRender.addEventListener("click", () => {
+    squares = document.querySelector("#squares").value;
+    if (squares >= 0 && squares <= 100) {
+        render(squares);
+    } else {
+        alert("Number of squares can't exceed 100!");
+    }
+});
+
+colorSelect = document.querySelector("#color");
+colorSelect.addEventListener("input", (event) => {
+    color = event.target.value;
+})
 
 container.addEventListener("dragstart", (event) =>  event.preventDefault());
 container.addEventListener("mousedown", () => clicked = true);
 container.addEventListener("mouseup", () => clicked = false);
-
 
 function draw(event, color) {
     if (clicked || event.type === "mousedown") {
@@ -14,18 +29,20 @@ function draw(event, color) {
     }
 }
 
-for (let i = 0; i < squares; i++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.toggle("row");
-    for (let j = 0; j < squares; j++) {
-        let colDiv = document.createElement("div");
-        colDiv.classList.toggle("col");
-        colDiv.style.width = "100%";
-        colDiv.style.height = "100%";
-        colDiv.addEventListener("mousedown", (event) => draw(event, color));
-        colDiv.addEventListener("mouseover", (event) => draw(event, color));
-        rowDiv.appendChild(colDiv);
+function render(squares) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-    container.appendChild(rowDiv);
+    for (let i = 0; i < squares; i++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.toggle("row");
+        for (let j = 0; j < squares; j++) {
+            let colDiv = document.createElement("div");
+            colDiv.classList.toggle("col");
+            colDiv.addEventListener("mousedown", (event) => draw(event, color));
+            colDiv.addEventListener("mouseover", (event) => draw(event, color));
+            rowDiv.appendChild(colDiv);
+        }
+        container.appendChild(rowDiv);
+    }
 }
-
